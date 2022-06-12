ee03d027c512c9521018a93a82d1e33d11bcbf0d
Ramesh Sannareddy <rsannareddy@gmail.com>
2022-06-12 19:34:40 +0530

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ var principal = document.getElementById("principal").value;
 var rate = document.getElementById("rate").value;  
 var years = document.getElementById("years").value;
 var amount = principal * years * rate /100;
+var b = document.getElementById('principal');
 if(principal <= 0){
     alert("Enter a positive numbers: ");
+    b.focus();
 }else{
     var year = new Date().getFullYear()+parseInt(years);
     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
